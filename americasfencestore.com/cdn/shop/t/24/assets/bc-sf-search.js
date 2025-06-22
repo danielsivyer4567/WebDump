4d69154ac1b9e14fc0182ea3cfbdfc57367d6660
danielsivyer4567 <danielsivyer4567@gmail.com>
2025-06-22 20:39:18 +1000

    Remove obsolete files including custom CSS, JavaScript, HTML, and various icons from the project, streamlining the codebase and eliminating unused resources.

---
+++ americasfencestore.com/cdn/shop/t/24/assets/bc-sf-search.js
@@ -0,0 +1,2 @@
+var bcSfSearchSettings={search:{}};BCSfFilter.prototype.customizeSuggestion=function(suggestionElement,searchElement,searchBoxId){};
+//# sourceMappingURL=/cdn/shop/t/24/assets/bc-sf-search.js.map?v=87487182149882261081692386824
